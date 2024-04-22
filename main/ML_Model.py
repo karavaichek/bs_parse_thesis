3a60393e1e2efa700f8def9ed872cf03687332ee
Denis Karakay <75396770+karavaichek@users.noreply.github.com>
2024-04-23 01:57:56 +0300

    new files

--- main/ML_Model.py
+++ main/ML_Model.py
@@ -7,13 +7,19 @@ from sklearn.metrics import mean_squared_error
 import json
 import csv
 from functools import reduce
+import matplotlib.pyplot as plt
 
 # Load your dataset into a pandas DataFrame
 with open(f'E:/Thesis/Excel files/Output_dataset/full_dataset.json', 'r') as full_dataset:
     data_set = json.load(full_dataset)
+
+
+
 data = pd.DataFrame.from_dict(data_set,orient='index', columns=['Revenue', 'COGS', 'EBITDA', 'Op. Revenue', 'Sector', 'Cash', 'inventory',
                                'AR', 'AP', 'Current Debt', 'Current Assets', 'Current Liabilities',
-                               'EBITDA Margin', 'CCC'])  # Replace "your_dataset.csv" with the path to your dataset file
+                               'EBITDA Margin', 'CCC'])
+
+
 #data = data.transpose()
 print (data)
 # Preprocessing: Depending on your dataset and its characteristics, you might need to perform various preprocessing steps such as handling missing values, encoding categorical variables, scaling numerical features, etc.
@@ -22,8 +28,8 @@ data.dropna(inplace=True)  # Drop rows with missing values
 data = pd.get_dummies(data, columns=['Sector'])
 
 # Splitting data into features and target variable
-X = data.drop('CCC', axis=1)
-y = data['CCC']
+X = data.drop('EBITDA Margin', axis=1)
+y = data['EBITDA Margin']
 
 # Splitting data into train and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -34,7 +40,7 @@ X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
 # Training the neural network model
-model = MLPRegressor(hidden_layer_sizes=(100, 50, 25,10), activation='relu', solver='adam', random_state=42)
+model = MLPRegressor(hidden_layer_sizes=list(reversed(range (10,1011,50))), activation='relu', solver='adam', random_state=42)
 model.fit(X_train_scaled, y_train)
 
 # Predicting on the test set
@@ -43,3 +49,24 @@ y_pred = model.predict(X_test_scaled)
 # Evaluating the model
 mse = mean_squared_error(y_test, y_pred)
 print("Mean Squared Error:", mse)
+
+# Visualizing feature importances
+feature_importances = np.abs(model.coefs_[0])
+feature_names = X.columns
+
+# Taking the mean importance across neurons
+mean_importance = feature_importances.mean(axis=1)
+
+# Printing feature importances in a tabular format
+importance_df = pd.DataFrame(mean_importance, index=feature_names, columns=['Importance'])
+importance_df_sorted = importance_df.sort_values(by='Importance', ascending=False)
+print("Feature Importances:")
+print(importance_df_sorted)
+
+plt.figure(figsize=(10, 6))
+plt.barh(feature_names, feature_importances.mean(axis=1))
+plt.xlabel('Average Importance')
+plt.ylabel('Features')
+plt.title('Feature Importances')
+plt.show()
+
--- main/test_file.py
+++ main/test_file.py
@@ -1,52 +1,18 @@
 import json
 
+# Load your dataset into a pandas DataFrame
 with open(f'E:/Thesis/Excel files/Output_dataset/full_dataset.json', 'r') as full_dataset:
     data_set = json.load(full_dataset)
 
-tickers = ['ACN', 'ADBE', 'AFL', 'ABNB', 'AKAM', 'ARE', 'ALL', 'AXP', 'AIG', 'AMT', 'AMP', 'ANSS', 'AON', 'ACGL', 'AJG',
-           'AIZ', 'ADSK', 'ADP', 'AVB', 'BAC', 'BK', 'BRK-B', 'BLK', 'BX', 'BKNG', 'BXP', 'BR', 'BRO', 'CPT', 'COF',
-           'CBOE', 'CBRE', 'CNC', 'CHRW', 'SCHW', 'CHTR', 'CB', 'CI', 'CINF', 'C', 'CFG', 'CME', 'CTSH', 'CMCSA', 'CMA',
-           'CSGP', 'CCI', 'DAY', 'DLR', 'DFS', 'DG', 'DLTR', 'DHI', 'EBAY', 'EA', 'ELV', 'EPAM', 'EQT', 'EFX', 'EQIX',
-           'EQR', 'ETSY', 'EG', 'EXPE', 'EXPD', 'EXR', 'FDS', 'FICO', 'FRT', 'FIS', 'FITB', 'FI', 'FLT', 'FOXA', 'FOX',
-           'BEN', 'IT', 'GEN', 'GPN', 'GL', 'GS', 'HIG', 'PEAK', 'HLT', 'HST', 'HUM', 'HBAN', 'ICE', 'IPG', 'INTU',
-           'IVZ', 'INVH', 'IQV', 'IRM', 'JKHY', 'J', 'JPM', 'KEY', 'KIM', 'L', 'LOW', 'MTB', 'MKTX', 'MAR', 'MMC', 'MA',
-           'MTCH', 'META', 'MET', 'MAA', 'MOH', 'MCO', 'MS', 'MSCI', 'NDAQ', 'NFLX', 'NTRS', 'ODFL', 'ORCL', 'PANW',
-           'PAYX', 'PYPL', 'PNC', 'PFG', 'PGR', 'PLD', 'PRU', 'PTC', 'PSA', 'RJF', 'O', 'REG', 'RF', 'RHI', 'SPGI',
-           'CRM', 'SBAC', 'NOW', 'SPG', 'STT', 'SYF', 'TROW', 'TSCO', 'TRV', 'TFC', 'TYL', 'USB', 'UBER', 'UDR', 'UNH',
-           'VTR', 'VRSN', 'VRSK', 'VICI', 'V', 'WRB', 'WBD', 'WFC', 'WELL', 'WTW', 'YUM', 'ZION']
+keys = data_set.copy().keys()
 
-for tkr in tickers:
-    for i in range(2020, 2024):
-        if data_set[tkr][str(i)]:
-            try:
-                data = data_set[tkr][str(i)]['bs']['Current Assets']
-            except KeyError:
-                print(tkr, 'Current Assets')
-            try:
-                data = data_set[tkr][str(i)]['bs']['Current Liabilities']
-            except KeyError:
-                print (tkr, 'Current Liabilities')
-            try:
-                data = data_set[tkr][str(i)]['pl']['Total Revenue']
-            except KeyError:
-                print (tkr, 'Total Revenue')
-            try:
-                data = data_set[tkr][str(i)]['bs']['Accounts Receivable']
-            except KeyError:
-                print(tkr, 'Accounts Receivable')
-            try:
-                data = data_set[tkr][str(i)]['bs']['Accounts Payable']
-            except KeyError:
-                print(tkr, 'Accounts Payable')
-            try:
-                data = data_set[tkr][str(i)]['bs']['Inventory']
-            except KeyError:
-                try:
-                    data_set[tkr][str(i)]['bs'].update({'Inventory':0})
-                except KeyError:
-                    pass
+for elem in keys:
+    try:
+        if data_set[elem][13] < 0 or 10<data_set[elem][12]<-10:
+            data_set.pop(elem)
+            print ('popped')
+    except KeyError:
+        pass
 
-
-with open(f'E:/Thesis/Excel files/Output_dataset/full_dataset.json', 'w') as f:
-    json.dump(data_set,f)
-
+with open(f'E:/Thesis/Excel files/Output_dataset/full_dataset.json', 'w') as write:
+    json.dump(data_set,write)
